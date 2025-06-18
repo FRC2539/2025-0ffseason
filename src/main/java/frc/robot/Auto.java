d3f1d18bd92b1b9c0540f185f16fc5e40b4afbba
trainrechs <jamesgormleyrechs@gmail.com>
2025-06-18 18:41:35 -0400

    auto code

---
+++ src/main/java/frc/robot/Auto.java
@@ -0,0 +1,109 @@
+package frc.robot;
+
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.trajectory.PathPlannerTrajectory;
+import com.pathplanner.lib.util.PathPlannerLogging;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.constants.AligningConstants;
+import frc.robot.constants.GlobalConstants;
+import frc.robot.constants.VisionConstants;
+import frc.robot.subsystems.drivetrain.CommandSwerveDrivetrain;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.json.simple.parser.ParseException;
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+
+public class Auto {
+    private final LoggedDashboardChooser<Command> autoChooser;
+    private RobotConfig config; // PathPlanner robot configuration
+
+
+
+    private final SwerveRequest.ApplyRobotSpeeds pathApplyRobotSpeeds = new SwerveRequest.ApplyRobotSpeeds();
+
+    // *NEW
+    private final Field2d m_trajectoryField = new Field2d();
+
+    public Auto(CommandSwerveDrivetrain drivetrain) {
+        setUpPathPlanner(drivetrain);
+        autoChooser = new LoggedDashboardChooser<>("Auto Routine", AutoBuilder.buildAutoChooser());
+        SmartDashboard.putData("Auto Path", m_trajectoryField);
+    }
+
+    public Command getAuto() {
+        if (AutoBuilder.isConfigured()) {
+            return autoChooser.get();
+        } else {
+            return Commands.none();
+        }
+    }
+
+    public void setUpPathPlanner(frc.robot.subsystems.drivetrain.CommandSwerveDrivetrain drivetrain) {
+        try {
+            config = RobotConfig.fromGUISettings();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        AutoBuilder.configure(
+                drivetrain::getRobotPose,
+                drivetrain::resetPose,
+                () -> drivetrain.getState().Speeds,
+                (speeds, feedforwards) ->
+                        drivetrain.setControl(pathApplyRobotSpeeds.withSpeeds(speeds)
+                            .withWheelForceFeedforwardsX(feedforwards.robotRelativeForcesXNewtons())
+                            .withWheelForceFeedforwardsY(feedforwards.robotRelativeForcesYNewtons())),
+                new PPHolonomicDriveController( // PPHolonomicController is the built in path
+                        // following controller for holonomic drive trains
+                        new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                        new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+                        ),
+                config,
+                () -> {
+                    // Boolean supplier that controls when the path will be mirrored for the red
+                    // alliance
+                    Optional<Alliance> alliance = DriverStation.getAlliance();
+                    if (alliance.isPresent()) {
+                        return alliance.get() == DriverStation.Alliance.Red;
+                    }
+                    return false;
+                },
+                drivetrain);
+
+        // Logging callback for target robot pose
+        PathPlannerLogging.setLogTargetPoseCallback(
+                (pose) -> {
+                    // Do whatever you want with the pose here
+                    m_trajectoryField.getObject("Robot").setPose(pose);
+                });
+    }
+}
